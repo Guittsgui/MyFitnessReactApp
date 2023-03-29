3a3f694bff921f1a50eb50693b7052710af9c131
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-03-29 12:26:23 -0300

    controlled inputs da login area

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -1,16 +1,35 @@
 import * as C from "./../Register/styled"
 import { StyledButton } from "../../components/Button"
 import { StyledInput } from "../../components/Input"
+import {useState} from 'react'
+import { isEmailValid } from "../../utils/isEmailValid"
 function Login() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleChangeEmail(e){
+    setEmail(e.target.value)
+  }
+  function handleChangePass(e){
+    setPassword(e.target.value)
+  }
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log(isEmailValid(email))
+    
+    alert(`O usuário ${email} com senha ${password} tentou logar`)
+  }
+
   return (
     <C.StyledBody>
       <C.StyledRegisterContainer>
 
         <C.StyledFormArea>
           <h2> Realize seu Login </h2>
-          <StyledInput placeholder='Informe seu Email :'/>
-          <StyledInput placeholder='Informe sua Senha :'/>
-          <StyledButton> Login </StyledButton>
+          <StyledInput placeholder='Informe seu Email :' type="email" value={email} onChange={handleChangeEmail}/>
+          <StyledInput placeholder='Informe sua Senha :' type="password" value={password} onChange={handleChangePass}/>
+          <StyledButton onClick={handleSubmit}> Login </StyledButton>
         </C.StyledFormArea>
         <C.StyledBanner>
           <div className='alignBanner'>
