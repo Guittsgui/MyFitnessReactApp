e7c268ae821355ef070e114b887ccae13d6360cb
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-04-03 19:44:58 -0300

    layout usuário finalizado, comecando IMC

--- src/routes/ImcCalculator/index.jsx
+++ src/routes/ImcCalculator/index.jsx
@@ -1,6 +1,10 @@
+import * as S from './style'
+
 const ImcCalculator = () => {
 
-
+    return <S.Body>
+        <h2> Testando</h2>
+    </S.Body>
 }
 
 export default ImcCalculator
--- src/routes/ImcCalculator/style.jsx
+++ src/routes/ImcCalculator/style.jsx
@@ -1,2 +1,5 @@
 import styled from 'styled-components'
 
+export const Body = styled.div`
+    height: 700px;
+`;
