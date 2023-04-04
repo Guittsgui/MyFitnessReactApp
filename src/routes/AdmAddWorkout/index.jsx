0cebeb73f620b1cac294bee7f9293c7b236eeac8
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-04-04 19:52:52 -0300

    bugou o useeffect

--- src/routes/AdmAddWorkout/index.jsx
+++ src/routes/AdmAddWorkout/index.jsx
@@ -1,10 +1,25 @@
 import * as S from './style'
+import { useParams } from 'react-router-dom'
+import { useState , useEffect } from 'react'
+import {api} from '../../api'
 
 const AdmAddWorkout = () => {
     
+    const {id} = useParams()
 
+    useEffect(()=> {
+        const loadUser = async () =>{
+            const newUser = await api.getUserByID(id)
+            setUsuario(newUser)
+            console.log(newUser)
+        }
+        loadUser()    
+    },[])
+
+    
     return <S.Body>
-        <h1> Add Workout </h1>
+        <h1> Add Workout  </h1>
+        
     </S.Body>
 
 
