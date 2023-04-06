892d15f3a8953a130fca67fb768a78c7db0d0d05
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-04-05 23:35:20 -0300

    nav/menu ajustado o UI

--- src/components/HeaderUser/style.jsx
+++ src/components/HeaderUser/style.jsx
@@ -1,26 +1,28 @@
 import styled from 'styled-components'
 
 export const StyledHeaderUser = styled.nav`
-    height: 80px;
+    height: 50px;
     background: rgb(43,162,93);
     background: radial-gradient(circle, rgba(43,162,93,1) 0%, rgba(17,60,5,1) 100%);
     display: flex;
     align-items: center;
     justify-content: space-around;
     font-size: 25px;
+    box-shadow: 2px 2px 2px #00000083;
+
 
     a{
         text-decoration: none;
         color: white;
         &:hover{
-            color: yellowgreen;
-            border-bottom: 2px solid yellowgreen;
-            padding-bottom: 10px;
+            color: #dafa99;
+            border-bottom: 1px solid yellowgreen;
+            padding-bottom: 2px;
         }
     }
     .active{
-        color: yellowgreen;
-        border-bottom: 2px solid yellowgreen;
-        padding-bottom: 10px;
+        color: #dafa99;
+        border-bottom: 1px solid yellowgreen;
+        padding-bottom: 2px;
     }
 `;
